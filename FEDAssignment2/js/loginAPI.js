034a2a8e8bc48a3ebef1ecc17f948983fbc8e9af
FakeQwek <133119964+FakeQwek@users.noreply.github.com>
2024-01-31 18:21:21 +0800

    Synchronising file structure
    Extracted files from FEDAssignment folder

---
+++ FEDAssignment2/js/loginAPI.js
@@ -0,0 +1,27 @@
+let el = document.getElementById("signup");
+
+function submit() {
+    let username = document.getElementById("username");
+
+    let password = document.getElementById("password");
+
+    let jsondata = {
+        "username": username.value,
+        "password": password.value
+    }
+
+    fetch("https://twitchaccounts-179c.restdb.io/rest/accounts", {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "x-apikey": "65b735c95a960f4dd87795a3",
+        "Cache-Control": "no-cache"
+    },
+    body: JSON.stringify(jsondata)
+    })
+    .then(response => {
+        return response.json();
+    })
+}
+
+el.addEventListener("click", submit);
