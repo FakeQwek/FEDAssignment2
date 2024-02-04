dfc7069cebb59be2c5851d7266486d169ee10993
chinteck2 <S10255880@connect.np.edu.sg>
2024-02-04 21:08:40 +0800

    new file:   FEDAssignment2/js/quizEnd.js new file:   FEDAssignment2/quiz-end.html

---
+++ FEDAssignment2/js/quizEnd.js
@@ -0,0 +1,68 @@
+let currScore = localStorage.getItem("score")
+if (currScore > 9) {
+    currScore = 9;
+}
+let jsondata = {
+    "username": "test2",
+    "score": currScore
+}
+
+
+fetch("https://twitchaccounts-179c.restdb.io/rest/scoreboard", {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "x-apikey": "65b735c95a960f4dd87795a3",
+        "Cache-Control": "no-cache"
+    },
+    body: JSON.stringify(jsondata)
+    })
+    .then(response => {
+        return response.json();
+    })
+
+fetch("https://twitchaccounts-179c.restdb.io/rest/scoreboard", {
+    method: "GET",
+    headers: {
+        "Content-Type": "application/json",
+        "x-apikey": "65b735c95a960f4dd87795a3",
+        "Cache-Control": "no-cache"
+    }
+    })
+    .then(response => {
+        return response.json();
+    })
+    .then(data => {
+        let length = 0;
+        if (data.length > 10) {
+            length = 10;
+        }
+        else {
+            length = data.length;
+        }
+
+        for (let i = 0; i < length; i++) {
+            let next = document.getElementById("next");
+            next.addEventListener("click", func => {
+                let page = document.getElementById("page");
+                pageHTML = `<div class="d-flex row px-3 sm-w25-100 my-2">
+                                <div class="col-7 d-flex">
+                                    <h3>TestUser</h3>
+                                </div>
+                                <div class="col d-flex justify-content-end">
+                                    <h3>` + data[i].score + `/9</h3>
+                                </div>
+                            </div>`
+                if (i == 0) {
+                    page.innerHTML = `<div id="leaderboard" class="d-flex align-items-center vh-100 flex-column py-5">
+                                        <h1 class="font-100 mb-4 px-3 border-bottom">Leaderboard</h1>
+                                      </div>`
+                    let leaderboard = document.getElementById("leaderboard");
+                    leaderboard.insertAdjacentHTML("beforeend", pageHTML);
+                }
+                else {
+                    leaderboard.insertAdjacentHTML("beforeend", pageHTML);
+                }
+            })
+        }
+    })
